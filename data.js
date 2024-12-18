b438b2425568320100a0362f50938f9ec6610379
Sonali <67572201+sonali-tandon@users.noreply.github.com>
2024-12-18 03:37:23 -0500

    Add external data file

---
+++ data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://scrimba.com/links/travel-journal-japan-image-url"
+    },
+    {
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://www.google.com/maps/place/Sydney+Opera+House",
+        startDate: "27 May, 2023",
+        endDate: "08 Jun, 2023",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+        imageUrl: "https://scrimba.com/links/travel-journal-australia-image-url"
+    },
+    {
+        title: "Geirangerfjord",
+        location: "Norway",
+        googleMapsUrl: "https://www.google.com/maps/place/Geirangerfjord",
+        startDate: "01 Oct, 2024",
+        endDate: "18 Nov, 2024",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        imageUrl: "https://scrimba.com/links/travel-journal-norway-image-url"
+    }
+]
